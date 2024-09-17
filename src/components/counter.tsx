9d186eba88bb7e291be598d7e3b41a1f6bcfff55
Manjunath s <111382056+manjunath-dalavaye@users.noreply.github.com>
2024-09-17 09:50:45 +0530

    redux_flow

---
+++ src/components/counter.tsx
@@ -0,0 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux';
+import {DispatchState,RootState} from '../store';
+import { decrement, increment } from './counterSlice';
+
+
+const Counter=()=>{
+    const count=useSelector((state:RootState) => state.counter.value)
+    const dispatch=useDispatch<DispatchState>();
+
+    return (
+
+        <>
+        <h2>Count:{count}</h2>
+        <button 
+        onClick={()=>{
+            dispatch(increment());
+            }}>Increment</button>
+    
+        <button
+        onClick={()=>{
+            dispatch(decrement());
+            }}>Decrement</button>
+       
+        </>
+    )
+}
+
+export default Counter;
---
+++ src/components/counterSlice.ts
@@ -0,0 +1,24 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+
+interface ConterState {
+    value: number;
+}
+
+const initialState:ConterState={value:0}
+
+const ConterSlice = createSlice({
+    name:"CounterSlice",
+    initialState,
+    reducers:{
+        increment:(state)=>{
+            state.value+=1
+        },
+        decrement:(state)=>{
+            state.value-=1
+        },
+    }
+})
+
+export const {increment,decrement} = ConterSlice.actions;
+export default ConterSlice.reducer;
